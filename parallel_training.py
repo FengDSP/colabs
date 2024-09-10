085ce2b93f35a2aec3528266c5c048769ebb3789
Feng Li <fenglidaniel@gmail.com>
2024-09-10 05:35:30 +0000

    Train MLP with muParam

--- parallel_training.py
+++ parallel_training.py
@@ -1,17 +1,16 @@
 import time
-import math
 
 import numpy as np
 import torch.nn.functional as F
 from torchvision import datasets, transforms
 import torch
 from torch import nn
-from torch.optim import SGD
-import seaborn as sns
 import pandas as pd
 from concurrent.futures import ProcessPoolExecutor
+import multiprocessing
 
 from mup import MuSGD, get_shapes, set_base_shapes, make_base_shapes, MuReadout
+
 
 batch_size = 64
 epochs = 20
@@ -19,26 +18,7 @@ log_interval = 300
 nonlin = torch.relu
 criterion = F.cross_entropy
 data_dir = "/tmp"
-
-torch.manual_seed(1)
-device = torch.device("cuda")
-
-transform = transforms.Compose(
-    [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
-)
-trainset = datasets.CIFAR10(
-    root=data_dir, train=True, download=True, transform=transform
-)
-train_loader = torch.utils.data.DataLoader(
-    trainset, batch_size=batch_size, shuffle=True, num_workers=1
-)
-testset = datasets.CIFAR10(
-    root=data_dir, train=False, download=True, transform=transform
-)
-test_loader = torch.utils.data.DataLoader(
-    testset, batch_size=batch_size, shuffle=False, num_workers=2
-)
-
+base_shapes_path = './demo_width256.bsh'
 
 def train(
     model,
@@ -103,18 +83,65 @@ class MLP(nn.Module):
         return self.fc_3(out) * self.output_mult
 
 
-def train_with(width, log2lr, epochs=20):
+class muMLP(nn.Module):
+    def __init__(self, width=128, num_classes=10, nonlin=F.relu, output_mult=1.0, input_mult=1.0):
+        super(muMLP, self).__init__()
+        self.nonlin = nonlin
+        self.input_mult = input_mult
+        self.output_mult = output_mult
+        self.fc_1 = nn.Linear(3072, width, bias=False)
+        self.fc_2 = nn.Linear(width, width, bias=False)
+        self.fc_3 = MuReadout(width, num_classes, bias=False, output_mult=self.output_mult)
+        self.reset_parameters()
+    
+    def reset_parameters(self):
+        nn.init.kaiming_normal_(self.fc_1.weight, a=1, mode='fan_in')
+        # self.fc_1.weight.data /= self.input_mult**0.5  # Why this doesn't work in multi processing?
+        nn.init.kaiming_normal_(self.fc_2.weight, a=1, mode='fan_in')
+        nn.init.zeros_(self.fc_3.weight)
+
+    def forward(self, x):
+        out = self.nonlin(self.fc_1(x) * self.input_mult**0.5)
+        out = self.nonlin(self.fc_2(out))
+        return self.fc_3(out)
+
+
+def train_with(width, log2lr, epochs=20, mup=True):
+    torch.manual_seed(1)
+    transform = transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))]
+    )
+    trainset = datasets.CIFAR10(
+        root=data_dir, train=True, download=True, transform=transform
+    )
+    train_loader = torch.utils.data.DataLoader(
+        trainset, batch_size=batch_size, shuffle=True, num_workers=1
+    )
+    # testset = datasets.CIFAR10(
+    #     root=data_dir, train=False, download=True, transform=transform
+    # )
+    # test_loader = torch.utils.data.DataLoader(
+    #     testset, batch_size=batch_size, shuffle=False, num_workers=2
+    #     )
+
+    device = torch.device("cuda")
     train_name = f"width={width} log2lr={log2lr}"
     print(f"Training starting {train_name}")
     res = []
     try:
-        mlp_model = MLP(width=width).to(device)
+        if mup:
+            mlp_model = muMLP(width=width).to(device)
+            set_base_shapes(mlp_model, base_shapes_path)
+            optimizer_class = MuSGD
+        else:
+            mlp_model = MLP(width=width).to(device)
+            optimizer_class = torch.optim.SGD
         for epoch in range(epochs):
             train_loss = train(
                 mlp_model,
                 device=device,
                 train_loader=train_loader,
-                optimizer=torch.optim.SGD(mlp_model.parameters(), lr=2**log2lr),
+                optimizer=optimizer_class(mlp_model.parameters(), lr=2**log2lr),
                 train_name=f"{train_name} epoch={epoch}",
             )
             res.append({
@@ -130,21 +157,37 @@ def train_with(width, log2lr, epochs=20):
     return res
 
 
-print("Running trainers ...")
-num_trains = 40
-results = []
-with ProcessPoolExecutor(max_workers=num_trains) as pool:
-    futures = []
-    for width in [1024, 512, 256, 128, 64]:
-        for log2lr in np.linspace(-8, -1, 8):
-            future = pool.submit(train_with, width, log2lr)
-            futures.append(future)
-    print("Waiting for results ...")
-    for f in futures:
-        try:
-            results.extend(f.result())
-        except Exception as e:
-            print(f"Task generated an exception: {e}")
-            raise
-df = pd.DataFrame(results)
-df.to_json("cifar10.jsonl", orient="records", lines=True)
+def main():
+    base_shapes = get_shapes(MLP(width=256, nonlin=torch.relu))
+    delta_shapes = get_shapes(
+        # just need to change whatever dimension(s) we are scaling
+        MLP(width=256+1, nonlin=torch.relu)
+    )
+    make_base_shapes(base_shapes, delta_shapes, savefile=base_shapes_path)
+
+    # Test a single process trainer run.
+    # train_with(64, -1)
+
+    print("Running trainers ...")
+    num_trains = 40
+    results = []
+    with ProcessPoolExecutor(max_workers=num_trains) as pool:
+        futures = []
+        for width in [64]:  # [8192, 4096, 2048]:
+            for log2lr in np.linspace(-8, -1, 8):
+                future = pool.submit(train_with, width, log2lr)
+                futures.append(future)
+        print("Waiting for results ...")
+        for f in futures:
+            try:
+                results.extend(f.result())
+            except Exception as e:
+                print(f"Task generated an exception: {e}")
+                raise
+    df = pd.DataFrame(results)
+    df.to_json("cifar10_mu1.jsonl", orient="records", lines=True)
+
+
+if __name__ == "__main__":
+    multiprocessing.set_start_method('spawn')
+    main()
